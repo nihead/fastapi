28de06e17be29f9aa9cfc565249bb31648635841
nihead <69080023+nihead@users.noreply.github.com>
2024-07-13 12:23:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+
+
+@app.get("/version")
+async def root():
+    return {"This is cloned version"}
